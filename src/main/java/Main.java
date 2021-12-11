16f728467c9e619f9f9b38a6fb12ac49e3085d4c
Ivanova Irina <chuda913@ya.ru>
2021-12-11 12:41:15 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "maybe your ad could be here";
+        Function<String, List<String>> function = (a) ->
+                Arrays.stream(a.split(" ")).sorted()
+                        .distinct()
+                        .toList();
+        function.apply(text).forEach(System.out::println);
+    }
+}
+//Использовала реализацию функционального интерфейса Function и монады в виде Stream API.
